79774186ddb876e01d64e30fb22affa0f831e91e
akquigley <98438324+akquigley@users.noreply.github.com>
2022-07-14 13:53:24 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+function getAreaOfCircle(radius) {
+    return 3.14 * (radius * 2);
+}
+
+getAreaOfCircle(15);
+
+function getCircumferenceOfCircle(radius) {
+    return 2 * 3.14 * radius;
+}
+
+getCircumferenceOfCircle(15);
+
+function getAreaOfSquare(side) {
+    return side * side;
+}
+
+getAreaOfSquare(5);
+
+function getAreaOfTriangle(base, height) {
+    return (1/2) * base * height;
+}
+
+getAreaOfTriangle(11, 8);
